c2b0fe092b679875f813990e7d4ad5f7bafdec22
VoidKingdom <gxzd999@gmail.com>
2020-11-24 11:35:59 +0000

    Updated Runner Database

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,1 +1,6 @@
-INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `ims`.`customer` (`firstname`, `surname`) VALUES ('jordan', 'patel');
+INSERT INTO `ims`.`item` (`name`, `value`) VALUES ('figure', 99.99);
+INSERT INTO `ims`.`item` (`name`, `value`) VALUES ('cup', 4.99);
+INSERT INTO `ims`.`order` (`user_id`) VALUES (1);
+INSERT INTO `ims`.`order_item` (`order_id`, `item_id`, `quantity`) VALUES (1, 1, 5);
+INSERT INTO `ims`.`order_item` (`order_id`, `item_id`, `quantity`) VALUES (1, 2, 3);
--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,9 +1,22 @@
 drop schema ims;
 CREATE SCHEMA IF NOT EXISTS `ims`;
 USE `ims` ;
-CREATE TABLE IF NOT EXISTS `ims`.`customers` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `first_name` VARCHAR(40) NULL DEFAULT NULL,
-    `surname` VARCHAR(40) NULL DEFAULT NULL,
-    PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS `ims`.`customer` (
+                                                user_id	INT(16) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+                                                firstname TINYTEXT NOT NULL,
+                                                surname TINYTEXT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS `ims`.`item` (
+                                            item_id	INT(16) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+                                            name VARCHAR(40) NOT NULL,
+                                            value DECIMAL(5,2) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS `ims`.`order` (
+                                             order_id INT(16) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+                                             user_id	INT(16) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS `ims`.`order_item` (
+                                                  order_id INT(16) NOT NULL,
+                                                  item_id	INT(16) NOT NULL,
+                                                  quantity INT(8) NOT NULL
 );
